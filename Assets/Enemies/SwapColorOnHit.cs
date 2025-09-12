1b389be784394657f7a546351bd3641b416bf426
richarx <theorichard12@gmail.com>
2025-09-12 08:15:20 +0200

    swap enemy color on hit

---
+++ Assets/Enemies/SwapColorOnHit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class SwapColorOnHit : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer;
+        [SerializeField] private Color targetColor;
+        [SerializeField] private float duration;
+        
+        private Damageable damageable;
+        private Color startingColor;
+
+        private void Start()
+        {
+            startingColor = spriteRenderer.color;
+
+            damageable = GetComponent<Damageable>();
+            damageable.OnTakeDamage.AddListener(() =>
+            {
+                StopAllCoroutines();
+                StartCoroutine(SwapColors());
+            });
+        }
+
+        private IEnumerator SwapColors()
+        {
+            spriteRenderer.color = targetColor;
+            yield return new WaitForSeconds(duration);
+            spriteRenderer.color = startingColor;
+        }
+    }
+}
