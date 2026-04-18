e355fd1e86782f7b26b5ac7cf5af41b6cdcc05b5
richarx <theorichard12@gmail.com>
2026-04-18 05:58:56 +0200

    Mage Spawn Behaviour with damage zone

---
+++ Assets/Enemies/Damage Zones/Circle/CircleDamageZone.cs
@@ -0,0 +1,50 @@
+using PrimeTween;
+using UnityEngine;
+
+public class CircleDamageZone : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float spawnDuration;
+    [SerializeField] private Ease spawnEase;
+    [SerializeField] private float fillDuration;
+    [SerializeField] private Ease fillEase;
+    [SerializeField] private float despawnDuration;
+
+    [Space]
+    [SerializeField] private Color flashColor;
+    [SerializeField] private Color flashOutlineColor;
+    [SerializeField] private Color filledColor;
+    [SerializeField] private Color filledOutlineColor;
+
+    [Space]
+    [SerializeField] private Vector3 punchStrength;
+    [SerializeField] private float punchDuration;
+    [SerializeField] private float punchFrequency;
+
+    public void Setup()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.material = new Material(spriteRenderer.material);
+
+        spriteRenderer.material.SetFloat("_alpha", 0.0f);
+        spriteRenderer.material.SetFloat("_Radius", 0.0f);
+
+        int alphaId = Shader.PropertyToID("_alpha");
+        int rasiusId = Shader.PropertyToID("_Radius");
+        int inlineId = Shader.PropertyToID("_InlineThickness");
+        int inlineColorId = Shader.PropertyToID("_InlineColor");
+        int outlineColorId = Shader.PropertyToID("_OutlineColor");
+
+        Sequence.Create()
+        .Chain(Tween.MaterialProperty(spriteRenderer.material, alphaId, 1.0f, spawnDuration))
+        .Group(Tween.MaterialProperty(spriteRenderer.material, rasiusId, radius, spawnDuration, spawnEase))
+        .Chain(Tween.MaterialProperty(spriteRenderer.material, inlineId, radius, fillDuration, fillEase))
+        .Chain(Tween.MaterialColor(spriteRenderer.material, inlineColorId, flashColor, 0.05f))
+        .Group(Tween.MaterialColor(spriteRenderer.material, outlineColorId, flashOutlineColor, 0.05f))
+        .Chain(Tween.MaterialColor(spriteRenderer.material, inlineColorId, filledColor, 0.1f))
+        .Group(Tween.MaterialColor(spriteRenderer.material, outlineColorId, filledOutlineColor, 0.1f))
+        .Group(Tween.PunchScale(transform, punchStrength, punchDuration, punchFrequency))
+        .Chain(Tween.MaterialProperty(spriteRenderer.material, alphaId, 0.0f, despawnDuration));
+    }
+}
--- Assets/Enemies/Mage/Spawn/MageSpawn.cs
+++ Assets/Enemies/Mage/Spawn/MageSpawn.cs
@@ -1,25 +1,34 @@
+using System;
 using Enemies.Scripts.Behaviours;
 using PrimeTween;
 using UnityEngine;
 
 public class MageSpawn : MonoBehaviour, IEnemyBehaviour
 {
+    [SerializeField] private CircleDamageZone circleDamageZonePrefab;
+
     public void FixedUpdateBehaviour(EnemyController enemy)
     {
     }
 
     public void StartBehaviour(EnemyController enemy)
     {
-        enemy.shadowSprite.gameObject.SetActive(false);
-        enemy.sprite.gameObject.SetActive(false);
+        enemy.sprite.transform.position = Vector3.up * 30.0f;
+        enemy.shadowSprite.transform.localScale = Vector3.zero;
 
         Sequence.Create()
-            .ChainDelay(3.0f)
-            .ChainCallback(() => enemy.shadowSprite.gameObject.SetActive(true))
+            .ChainDelay(2.0f)
+            .ChainCallback(() => SpawnDamageZone(enemy.transform.position))
+            .ChainDelay(6f)
             .Chain(Tween.Alpha(enemy.shadowSprite, 1.0f, 1.0f))
-            .Group(Tween.Scale(enemy.shadowSprite.transform, new Vector3(0.1f, 0.1f, 1.0f), Vector3.one, 1.0f))
-            .ChainCallback(() => enemy.sprite.gameObject.SetActive(true))
-            .Chain(Tween.LocalPositionY(enemy.sprite.transform, 30.0f, 0.0f, 0.5f, Ease.OutBounce));
+            .Group(Tween.Scale(enemy.shadowSprite.transform, new Vector3(0.1f, 0.1f, 1.0f), Vector3.one, 0.5f))
+            .Group(Tween.LocalPositionY(enemy.sprite.transform, 30.0f, 0.0f, 0.5f, Ease.OutBounce));
+    }
+
+    private void SpawnDamageZone(Vector3 position)
+    {
+        CircleDamageZone circleDamageZone = Instantiate(circleDamageZonePrefab, position, Quaternion.Euler(new Vector3(90.0f, 0.0f, 0.0f)));
+        circleDamageZone.Setup();
     }
 
     public void StopBehaviour(EnemyController enemy)
