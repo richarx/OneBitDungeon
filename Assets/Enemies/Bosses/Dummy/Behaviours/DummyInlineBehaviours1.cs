808c424f32c3342c9b2e955725abf54fc0095154
cmeignan <clement.meignant@laposte.net>
2026-08-18 14:42:38 +0200

    Transition from legacy to Odin

--- Assets/Enemies/Bosses/Dummy/Behaviours/DummyInlineBehaviours1.cs
+++ Assets/Enemies/Bosses/Dummy/Behaviours/DummyInlineBehaviours1.cs
@@ -6,35 +6,6 @@ using Sirenix.Serialization;
 using UnityEngine;
 
 [Serializable]
-public sealed class DummyImmediateTransitionBehaviour : IEnemyBehaviour
-{
-    public void StartBehaviour(EnemyController enemy, BehaviourExecution execution)
-    {
-        execution.Complete();
-    }
-
-    public void UpdateBehaviour(EnemyController enemy)
-    {
-    }
-
-    public void FixedUpdateBehaviour(EnemyController enemy)
-    {
-    }
-
-    public void StopBehaviour(EnemyController enemy)
-    {
-    }
-
-    public void CancelBehaviour(EnemyController enemy)
-    {
-    }
-
-    public void SetSubBehaviourState(bool state)
-    {
-    }
-}
-
-[Serializable]
 public sealed class DummyCircleAttackBehaviour : IEnemyBehaviour
 {
     [OdinSerialize]
