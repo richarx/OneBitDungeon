b937e03dbdb4102f705c3dba45815bff552b7e84
richarx <theorichard12@gmail.com>
2026-08-18 20:05:41 +0200

    Change UI "Arrogance" text to "Insolence" when bar is filled

--- Assets/UI/Arrogance/ArroganceDisplay.cs
+++ Assets/UI/Arrogance/ArroganceDisplay.cs
@@ -1,5 +1,7 @@
+using System;
 using Player.Scripts;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,8 +11,11 @@ namespace UI.Arrogance
     {
         [Required, SerializeField] private Image arroganceBar;
         [SerializeField] private float smoothTime = 0.1f;
+        [SerializeField] private TextMeshProUGUI textMeshPro;
 
         private float velocity;
+
+        private bool isFilled;
 
         private void Update()
         {
@@ -21,6 +26,22 @@ namespace UI.Arrogance
             float target = PlayerStateMachine.instance.playerArrogance.NormalizedArrogance;
 
             arroganceBar.fillAmount = Mathf.SmoothDamp(current, target, ref velocity, smoothTime);
+
+            bool isCurrentlyFilled = target >= 1.0f;
+
+            if (isFilled != isCurrentlyFilled)
+            {
+                isFilled = isCurrentlyFilled;
+                UpdateText();
+            }
+        }
+
+        private void UpdateText()
+        {
+            if (isFilled)
+                textMeshPro.text = "<bounce a*3 s*2>Insolence</bounce>";
+            else
+                textMeshPro.text = "Arrogance";
         }
     }
 }
